fa209a95e95b7daa8dea6de2df9d568fd36aaf05
Martin <martinwjwilson@gmail.com>
2023-12-02 22:11:11 +0000

    Adding comments

--- 2023/day-02/game.py
+++ 2023/day-02/game.py
@@ -1,4 +1,11 @@
 class Game:
+    """
+    A class to represent a game.
+    A game has a list of rounds played.
+    rounds is a list containing a rounds of a game, with each round being a dictionary.
+    Each round has the name of colours and how many there were: {"red": 3, "blue": 4}
+    """
+
     def __init__(self, game_number: int, rounds_string: str):
         self.rounds_string = rounds_string
         self.number = game_number
@@ -39,6 +46,10 @@ class Game:
         self.__calculate_total_power()
 
     def __convert_rounds_string(self):
+        """
+        A method to convert the puzzle input string into valid format for the other methods to use.
+        :return:
+        """
         list_of_rounds = [x.strip() for x in self.rounds_string.split(';')]
         for current_round in list_of_rounds:
             current_round_squares = [x.strip() for x in current_round.split(',')]
@@ -50,10 +61,21 @@ class Game:
             self.rounds.append(converted_squares)
 
     def __calculate_total_power(self):
+        """
+        A method to calculate the total power for a game.
+        This is determined by the minimum required colours for a game multiplied together.
+        :return:
+        """
         self.__calculate_minimum_colours()
         self.total_power = self.minimum_red * self.minimum_green * self.minimum_blue
 
     def __calculate_minimum_colours(self):
+        """
+        A method to calculate and set the minimum number of cubes required for each colour.
+        For example the minimum number of red cubes needed will be determined by the
+        highest use in a round.
+        :return:
+        """
         for current_round in self.rounds:
             if "red" in current_round:
                 if current_round["red"] > self.minimum_red:
