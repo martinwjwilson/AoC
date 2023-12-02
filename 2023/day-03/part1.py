8bfb7488691d7b4f63419d4315db98a6ef96d8f5
Martin <martinwjwilson@gmail.com>
2023-12-02 22:45:28 +0000

    Initial file upload

---
+++ 2023/day-03/part1.py
@@ -0,0 +1,4 @@
+# get the input from the file
+f = open('test_input.txt', 'r')
+content = f.read()
+input_lines = content.split("\n")
---
+++ 2023/day-03/part2.py
@@ -0,0 +1,4 @@
+# get the input from the file
+f = open('input.txt', 'r')
+content = f.read()
+input_lines = content.split("\n")
