7c7df8abcfc9b652975245394f7895bd5a5d7c8f
Martin <martinwjwilson@gmail.com>
2024-12-02 13:12:01 +0000

    part 2 in progress

--- 2024/day-02/report.py
+++ 2024/day-02/report.py
@@ -1,20 +1,46 @@
+from calendar import different_locale
+from itertools import count
+
+
 class Report:
     def __init__(self, levels: [int]):
         self.levels = levels
 
     def is_safe(self) -> bool:
-        return self.all_levels_are_increasing_safely() or self.all_levels_are_decreasing_safely()
+        return (self.all_levels_are_increasing_safely(levels=self.levels.copy(), is_a_retest=False)
+                or self.all_levels_are_decreasing_safely(levels=self.levels.copy(), is_a_retest=False))
 
-    def all_levels_are_increasing_safely(self) -> bool:
-        for index, level in enumerate(self.levels):
+    def all_levels_are_increasing_safely(self, levels: [int], is_a_retest: bool) -> bool:
+        for index, level in enumerate(levels):
+            previous_level = levels[index-1]
             if index > 0:
-                if level <= self.levels[index - 1] or not (0 < abs(level - self.levels[index - 1]) < 4):
-                    return False
+                if (level <= previous_level
+                        or not self.difference_in_levels_is_safe(first_level=level,
+                                                                 second_level=previous_level)):
+                    if is_a_retest:
+                        return False
+                    else:
+                        if index != len(levels) - 1:
+                            levels.pop(index-1)
+                            return self.all_levels_are_increasing_safely(levels=levels, is_a_retest=True)
         return True
 
-    def all_levels_are_decreasing_safely(self) -> bool:
-        for index, level in enumerate(self.levels):
+    def all_levels_are_decreasing_safely(self, levels: [int], is_a_retest: bool) -> bool:
+        for index, level in enumerate(levels):
+            previous_level = levels[index-1]
             if index > 0:
-                if level >= self.levels[index - 1] or not (0 < abs(level - self.levels[index - 1]) < 4):
-                    return False
+                if (level >= previous_level
+                        or not self.difference_in_levels_is_safe(first_level=level,
+                                                                 second_level=previous_level)):
+                    if is_a_retest:
+                        return False
+                    else:
+                        if index != len(levels) - 1:
+                            levels.pop(index)
+                            return self.all_levels_are_decreasing_safely(levels=levels, is_a_retest=True)
         return True
+
+    @staticmethod
+    def difference_in_levels_is_safe(first_level: int, second_level: int) -> bool:
+        # print(f"first level: {first_level} minus {second_level}: {0 < abs(first_level - second_level) < 4}")
+        return 0 < abs(first_level - second_level) < 4
