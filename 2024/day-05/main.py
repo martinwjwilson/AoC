3ea2fea949d92eaa2fe8f31fed12d39d7d25a9cb
Martin <martinwjwilson@gmail.com>
2024-12-06 23:25:12 +0000

    part 1 further progress

--- 2024/day-05/main.py
+++ 2024/day-05/main.py
@@ -1,10 +1,12 @@
 from page_ordering_rule import PageOrderingRule
 
+
 def get_input() -> list[str]:
     # get the input from the file
     f = open('test_input.txt', 'r')
     content = f.read()
     return content.split("\n")
+
 
 def clean_puzzle_input(puzzle_input: [str]) -> [[str]]:
     sorted_input = []
@@ -18,6 +20,7 @@ def clean_puzzle_input(puzzle_input: [str]) -> [[str]]:
     sorted_input.append(temp_array)
     return sorted_input
 
+
 def create_all_page_ordering_rules(raw_page_ordering_rules: [str]) -> [PageOrderingRule]:
     _page_ordering_rules = []
     for rule in raw_page_ordering_rules:
@@ -27,13 +30,22 @@ def create_all_page_ordering_rules(raw_page_ordering_rules: [str]) -> [PageOrder
     return _page_ordering_rules
 
 
-def part_one_solution():
-    pass
+def create_all_pages_updates(raw_page_updates):
+    print(f"All pages: {raw_page_updates}")
+    _all_page_updates = []
+    for page_update in raw_page_updates:
+        _all_page_updates.append(page_update.split(","))
+    print(_all_page_updates)
+    return _all_page_updates
+
+
+def part_one_solution() -> str:
+    puzzle_input = get_input()
+    cleaned_puzzle_input = clean_puzzle_input(puzzle_input=puzzle_input)
+    page_ordering_rules = create_all_page_ordering_rules(cleaned_puzzle_input[0])
+    pages_updates = create_all_pages_updates(raw_page_updates=cleaned_puzzle_input[1])
+    return cleaned_puzzle_input
 
 
 if __name__ == '__main__':
-    puzzle_input = get_input()
-    cleaned_puzzle_input = clean_puzzle_input(puzzle_input=puzzle_input)
-    print(cleaned_puzzle_input)
-    page_ordering_rules = create_all_page_ordering_rules(cleaned_puzzle_input[0])
-
+    print(part_one_solution())
--- 2024/day-05/pages_to_update.py
+++
@@ -1,3 +0,0 @@
-class PagesToUpdate:
-    def __init__(self, pages: [int]):
-        pages
