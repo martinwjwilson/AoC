bacebd752143079ae1f8fcbeb372a4ce7e6f8e83
Martin <martinwjwilson@gmail.com>
2024-12-09 00:21:14 +0000

    Part 1 solution

--- 2024/day-05/main.py
+++ 2024/day-05/main.py
@@ -3,7 +3,7 @@ from page_ordering_rule import PageOrderingRule
 
 def get_input() -> list[str]:
     # get the input from the file
-    f = open('test_input.txt', 'r')
+    f = open('input.txt', 'r')
     content = f.read()
     return content.split("\n")
 
@@ -45,21 +45,37 @@ def part_one_solution() -> str:
     page_ordering_rules = create_all_page_ordering_rules(cleaned_puzzle_input[0])
     page_updates = create_all_pages_updates(raw_page_updates=cleaned_puzzle_input[1])
     correct_updates = get_all_correct_updates(ordering_rules=page_ordering_rules, page_updates=page_updates)
-    return ""
+    all_page_middle_numbers = get_all_page_middle_numbers(page_updates=correct_updates)
+    return sum(all_page_middle_numbers)
 
 
 def get_all_correct_updates(ordering_rules: [PageOrderingRule], page_updates: [[int]]) -> [[int]]:
     correct_updates = []
-    for updates in page_updates:
-        if
+    for update in page_updates:
+        if page_update_is_correct(ordering_rules=ordering_rules, page_updates=update):
+            correct_updates.append(update)
     return correct_updates
 
 
 def page_update_is_correct(ordering_rules: [PageOrderingRule], page_updates: [int]) -> bool:
-    # Go through each page and compare it against all the following pages to see if it follows all of the rules
+    # Go through each page and compare it against all the following pages to see if it follows all the rules
     for index, page in enumerate(page_updates):
-
+        for following_page in page_updates[index+1:]:
+            print(f"Current page: {page}, following page: {following_page}")
+            # If any of the numbers are the wrong way around
+            for rule in ordering_rules:
+                if rule.first_number == following_page and rule.second_number == page:
+                    print(f"This update failed on rule: {rule.first_number}|{rule.second_number}, where the current number is: {page} and the checked number is: {following_page}")
+                    return False
     return True
+
+
+def get_all_page_middle_numbers(page_updates: [[int]]) -> [int]:
+    all_page_middle_numbers = []
+    for page_update in page_updates:
+        middle_index = int((len(page_update) - 1)/2)
+        all_page_middle_numbers.append(page_update[middle_index])
+    return all_page_middle_numbers
 
 
 if __name__ == '__main__':
