dc2195533b9848c8dacbac1d35a370b373e32425
Martin <martinwjwilson@gmail.com>
2024-12-17 00:12:34 +0000

    Part 2 solution
    Nice and easy

--- 2024/day-07/equation.py
+++ 2024/day-07/equation.py
@@ -1,5 +1,6 @@
 from copy import deepcopy
 
+
 class Equation:
     def __init__(self, test_value: int, values: [int]):
         self.test_value = test_value
@@ -16,14 +17,21 @@ class Equation:
                 parent_nodes.append(value)
             else:
                 for node in parent_nodes:
-                    first_node, second_node = self.perform_all_operations_on_node(current_node=node,
-                                                                                  value=value)
-                    temp_parent_notes.extend([first_node, second_node])
+                    first_node, second_node, third_node = self.perform_all_operations_on_node(current_node=node,
+                                                                                              value=value)
+                    temp_parent_notes.extend([first_node, second_node, third_node])
                 parent_nodes = deepcopy(temp_parent_notes)
         return parent_nodes
 
     @staticmethod
-    def perform_all_operations_on_node(current_node, value) -> tuple[int, int]:
+    def perform_plus_and_multiplication_operations_on_node(current_node, value) -> tuple[int, int]:
         first_answer = current_node + value
         second_answer = current_node * value
         return first_answer, second_answer
+
+    @staticmethod
+    def perform_all_operations_on_node(current_node, value) -> tuple[int, int, int]:
+        first_answer = current_node + value
+        second_answer = current_node * value
+        third_answer = int(str(current_node) + str(value))
+        return first_answer, second_answer, third_answer
