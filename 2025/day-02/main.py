a970f1a1f00551dcaf449d3e36666ad88b4262b7
Martin Wilson <martinwjwilson@gmail.com>
2025-12-03 17:32:51 +0000

    Part 2 sub ids

--- 2025/day-02/main.py
+++ 2025/day-02/main.py
@@ -8,13 +8,24 @@ class Range:
         _all_invalid_ids = []
         for _int in full_range_of_integers:
             value_as_string = str(_int)
-            if len(value_as_string) % 2 == 0: # Check string can be split in half
-                firstpart, secondpart = (value_as_string[:len(value_as_string) // 2],
-                                         value_as_string[len(value_as_string) // 2:])
-                if firstpart == secondpart:
-                    _all_invalid_ids.append(_int)
+            if self.id_is_invalid(value_as_string):
+                _all_invalid_ids.append(_int)
         return _all_invalid_ids
 
+    def id_is_invalid(self, _id):
+        # [1,2,3,1,2,3,1,2,3]
+        is_valid = True
+        print(f"ID: {_id} -- Length = {len(_id)}")
+        # multiply the length of the sub id by the current index you're checking to check against other sections
+        for index in range(0, len(_id) - 1):
+            # Create sub array
+            sub_array = _id[0:index + 1]
+            print(f"Sub array: {sub_array}")
+            # Check the next sub array
+            for sub_index in range(0, len(_id),len(sub_array)):
+                print(f"Sub sub array: {_id[sub_index:sub_index + len(sub_array)]}")
+            print("")
+        return is_valid
 
     def get_full_range_as_integers(self):
         return list(range(self.start, self.end + 1))
